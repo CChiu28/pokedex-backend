7fc90b403972057ce8688b9509069d97b0f8185b
CChiu28 <ScorpiusDX@gmail.com>
2020-08-04 13:05:09 -0400

    Initial

---
+++ src/test/java/com/registrar/registrar2/RegistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registrar.registrar2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
